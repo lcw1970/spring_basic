1b2cc6291430da6a8f198ffcb4387d953c0b2378
이재현 <lcw7010@naver.com>
2025-10-14 16:40:58 +0900

    스프링 맛보기

---
+++ core/core/src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -0,0 +1,24 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RateDiscountPolicyTest {
+    RateDiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    @Test
+    @DisplayName("Vip는 10% 할인이 적용되어야 한다")
+    void vip_o() {
+        Member member = new Member(1L,"memberVip", Grade.VIP);
+
+        int discount = discountPolicy.discount(member,10000);
+
+        Assertions.assertThat(discount).isEqualTo(1000);
+    }
+
+}
